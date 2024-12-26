9f75278e3e20805ce2d0fef0c47717f0803cecb3
Pham Khai <117723165+ka31504@users.noreply.github.com>
2024-12-26 15:22:45 +0700

    Delete ThongTin directory

--- ThongTin/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\PostController;
-
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
