82a0cd52a3691c56c7cca71669b211de0c44621f
Pham Khai <117723165+ka31504@users.noreply.github.com>
2025-01-02 14:43:50 +0700

    Delete ThongTin directory

--- ThongTin/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
-Route::get('/', [BookController::class, 'index']);
-
-Route::get('/book', [BookController::class, 'index'])->name('book');
